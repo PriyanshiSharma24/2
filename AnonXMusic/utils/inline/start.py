6e9472aea457edd8a18318a0ca253ce5306e2a6b
𝖯𝗋𝗂𝗒𝖺𝗇𝗌𝗁𝗂 𝗌𝗁𝖺𝗋𝗆𝖺 ۵ 🇮🇳 <130631732+PriyanshiSharma24@users.noreply.github.com>
2025-06-29 11:47:22 +0530

    Update start.py
    Signed-off-by: 𝖯𝗋𝗂𝗒𝖺𝗇𝗌𝗁𝗂 𝗌𝗁𝖺𝗋𝗆𝖺 ۵ 🇮🇳 <130631732+PriyanshiSharma24@users.noreply.github.com>

--- AnonXMusic/utils/inline/start.py
+++ AnonXMusic/utils/inline/start.py
@@ -33,6 +33,5 @@ def private_panel(_):
             InlineKeyboardButton(text=_["S_B_6"], url="https://t.me/do_pal0"),
             InlineKeyboardButton(text=_["S_B_7"], await message.video(
         video="https://files.catbox.moe/e0d6wo.mp4"),
-        ],
-    ]
+       ]
     return buttons
