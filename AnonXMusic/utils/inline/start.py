953a7be1a09d29715b0244c3036c6a81fbc1056e
𝖯𝗋𝗂𝗒𝖺𝗇𝗌𝗁𝗂 𝗌𝗁𝖺𝗋𝗆𝖺 ۵ 🇮🇳 <130631732+PriyanshiSharma24@users.noreply.github.com>
2025-06-29 10:20:57 +0530

    Update start.py
    Signed-off-by: 𝖯𝗋𝗂𝗒𝖺𝗇𝗌𝗁𝗂 𝗌𝗁𝖺𝗋𝗆𝖺 ۵ 🇮🇳 <130631732+PriyanshiSharma24@users.noreply.github.com>

--- AnonXMusic/utils/inline/start.py
+++ AnonXMusic/utils/inline/start.py
@@ -31,7 +31,7 @@ def private_panel(_):
         ],
         [
             InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
-            InlineKeyboardButton(text=_["S_B_7"], callback_data="wait"),
+            InlineKeyboardButton(text=_["S_B_7"], user_id="6444481574"),
         ],
     ]
     return buttons
