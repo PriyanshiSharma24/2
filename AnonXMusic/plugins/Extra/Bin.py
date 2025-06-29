ee90773fbd07e62f6e50a7191a902854ad13ad2b
𝖯𝗋𝗂𝗒𝖺𝗇𝗌𝗁𝗂 𝗌𝗁𝖺𝗋𝗆𝖺 ۵ 🇮🇳 <130631732+PriyanshiSharma24@users.noreply.github.com>
2025-06-29 09:52:18 +0530

    Create Bin.py
    Signed-off-by: 𝖯𝗋𝗂𝗒𝖺𝗇𝗌𝗁𝗂 𝗌𝗁𝖺𝗋𝗆𝖺 ۵ 🇮🇳 <130631732+PriyanshiSharma24@users.noreply.github.com>

---
+++ AnonXMusic/plugins/Extra/Bin.py
@@ -0,0 +1,39 @@
+from SafoneAPI import SafoneAPI
+from pyrogram import *
+from pyrogram.types import *
+from AnonXMusic import app
+
+api = SafoneAPI()
+
+@app.on_message(filters.command(["bin", "ccbin", "bininfo"], [".", "!", "/"]))
+async def check_ccbin(client, message):
+    if len(message.command) < 2:
+        return await message.reply_text(
+            "<b>Please Give Me a Bin To\nGet Bin Details !</b>"
+        )
+    try:
+        await message.delete()
+    except:
+        pass
+    aux = await message.reply_text("<b>Checking ...</b>")
+    bin = message.text.split(None, 1)[1]
+    if len(bin) < 6:
+        return await aux.edit("<b>❌ Wrong Bin❗...</b>")
+    try:
+        resp = await api.bininfo(bin)
+        await aux.edit(f"""
+<b> ᴠᴀʟɪᴅ ✃ </b>
+
+<b>ʙᴀɴᴋ➪</b> <tt>{resp.bank}</tt>
+<b>ʙɪɴ➪</b> <tt>{resp.bin}</tt>
+<b>ᴄɴ➪</b> <tt>{resp.country}</tt>
+<b>ғʟᴀɢ➪</b> <tt>{resp.flag}</tt>
+<b>ɪsᴏ➪</b> <tt>{resp.iso}</tt>
+<b>ʟᴇᴠᴇʟ➪</b> <tt>{resp.level}</tt>
+<b>ᴘʀᴇᴘᴀɪᴅ➪</b> <tt>{resp.prepaid}</tt>
+<b>ᴛʏᴘᴇ➪</b> <tt>{resp.type}</tt>
+<b>ᴠᴇɴᴅᴏʀ➪</b> <tt>{resp.vendor}</tt>"""
+        )
+    except:
+        return await aux.edit(f"""
+🚫 BIN not recognized. Please enter a valid BIN.""")
